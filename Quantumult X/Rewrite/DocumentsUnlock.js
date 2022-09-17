5f5fae167be008f1e62850afcfa79ef1b2d64f32
Passerby_yun <106366379+baiji520@users.noreply.github.com>
2022-09-17 16:45:09 +0800

    Create DocumentsUnlock.js

---
+++ Quantumult X/Rewrite/DocumentsUnlock.js
@@ -0,0 +1,3 @@
+hostname = buy.itunes.apple.com
+
+https://buy.itunes.apple.com/verifyReceipt url script-response-body https://raw.githubusercontent.com/baiji520/All-In-One/main/Quantumult%20X/JS/DocumentsUnlock.js
