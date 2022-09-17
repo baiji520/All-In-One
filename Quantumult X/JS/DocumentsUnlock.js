daef29fee494dadc8a0c92ee8dc1cff1d7fb71b0
Passerby_yun <106366379+baiji520@users.noreply.github.com>
2022-09-18 03:58:30 +0800

    Update DocumentsUnlock.js

--- Quantumult X/JS/DocumentsUnlock.js
+++ Quantumult X/JS/DocumentsUnlock.js
@@ -11,7 +11,7 @@ obj = {
     "original_purchase_date" : "2022-09-17 04:27:28 Etc/GMT",
     "in_app" : [{
       "quantity": "1",
-      "product_id": "com.readdle.ReaddleDocsIPad.001",
+      "product_id": "com.readdle.ReaddleDocsIPad.year",
       "transaction_id": "730000766600650",
       "original_transaction_id": "730000766600650",
       "purchase_date": "2022-09-17 12:27:28 Etc/GMT",
