ad6f9948319060bfc5c50bbf1aac7fd284b13a3d
Passerby_yun <106366379+baiji520@users.noreply.github.com>
2022-09-21 12:28:38 +0800

    Update and rename PandaVpn-1.js to PandaVpnUnlock-1.js

--- Quantumult X/JS/PandaVpnUnlock-1.js
+++ Quantumult X/JS/PandaVpnUnlock-1.js
@@ -8,9 +8,9 @@ if (url.indexOf(vip) != -1) {
     obj.data.expireAt = "2077-07-07T12:17:13Z";
     obj.data.maxDeviceCount = 3;
     obj.data.expireRemindType = "MANDATORY",
-    obj.data.leftDays = 0;
-    obj.data.role = "NORMAL";
-    obj.data.rewardPoints = 240;
+    obj.data.leftDays = 3650;
+    obj.data.role = "pandavpn_1year";
+    obj.data.rewardPoints = 300;
     body = JSON.stringify(obj);
 }
 $done({body});
