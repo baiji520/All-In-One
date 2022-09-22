1584bf25a3c3dd8010ce1e32d3bf6203efee7b74
Passerby_yun <106366379+baiji520@users.noreply.github.com>
2022-09-22 19:30:03 +0800

    Update FeiYueVpnUnlock.js

--- Quantumult X/JS/FeiYueVpnUnlock.js
+++ Quantumult X/JS/FeiYueVpnUnlock.js
@@ -1,2 +1,2 @@
-body = $response.body.replace(/expireAt":\d+/g, 'expireAt":3392814600000').replace(/status":\d/g, 'status":3').replace(/type":\d/g, 'type":3').replace(/memberLevel":\d/g, 'memberLevel":1');
+body = $response.body.replace(/expiredAt":\d+/g, 'expireAt":3392814600000').replace(/status":\d/g, 'status":3').replace(/type":\d/g, 'type":3').replace(/memberLevel":\d/g, 'memberLevel":1');
 $done({body});
