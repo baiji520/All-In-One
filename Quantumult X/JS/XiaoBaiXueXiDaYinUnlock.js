6e565357d548e9d39f6b6b5af27f74d6c4759f81
Passerby_yun <106366379+baiji520@users.noreply.github.com>
2022-10-08 19:16:37 +0800

    Update XiaoBaiXueXiDaYinUnlock.js

--- Quantumult X/JS/XiaoBaiXueXiDaYinUnlock.js
+++ Quantumult X/JS/XiaoBaiXueXiDaYinUnlock.js
@@ -1,5 +1,2 @@
-body = $response.body.replace(/isSchoolAgeMember":\w+/g, 'isSchoolAgeMember":true').replace(/isPreschoolMember":\w+/g, 'isPreschoolMember":true').replace(/schoolAgeMember":\w+/g, 'schoolAgeMember":{
-          "__typename": "schoolAgeMember",
-          "expiresAt": "2077-07-07"
-        }')
+body = $response.body.replace(/isSchoolAgeMember":\w+/g, 'isSchoolAgeMember":true').replace(/isPreschoolMember":\w+/g, 'isPreschoolMember":true');
 $done({body});
